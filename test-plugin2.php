5de49a5a518fc1380ed90bab1d7333070b50bcf6
Andy Fragen <andy@thefragens.com>
2013-07-07 13:26:01 -0700

    add GitHub Plugin Header and remove old updater code

--- test-plugin2.php
+++ test-plugin2.php
@@ -2,6 +2,7 @@
 /*
 Plugin Name: Test Plugin2
 Plugin URI: https://github.com/afragen/test-plugin2/
+GitHub Plugin URI: https://github.com/afragen/test-plugin2/
 Description: This plugin is used for testing functionality of Github updating of plugins.
 Version: 0.4
 Author: Andy Fragen
@@ -9,15 +10,3 @@ License: GNU General Public License v2
 License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
-function my2_github_plugin_updater() {
-
-	if ( ! function_exists( 'github_plugin_updater_register' ) )
-		return false;
-
-	github_plugin_updater_register( array(
-		'owner'	=> 'afragen',
-		'repo'	=> 'test-plugin2',
-		'slug'	=> 'test-plugin2/test-plugin2.php', // defaults to the repo value ('repo/repo.php')
-	) );
-}
-add_action( 'plugins_loaded', 'my2_github_plugin_updater' );
