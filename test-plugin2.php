96ccb0c8923793d2faf31f09070bf21e0c401443
Andy Fragen <andy@thefragens.com>
2013-11-23 19:55:23 -0800

    Spacing align headers and version bump to test branch

--- test-plugin2.php
+++ test-plugin2.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: Test Plugin2
-Plugin URI: https://github.com/afragen/test-plugin2/
+Plugin Name:       Test Plugin2
+Plugin URI:        https://github.com/afragen/test-plugin2/
+Description:       This plugin is used for testing functionality of Github updating of plugins.
+Version:           0.4.2
+Author:            Andy Fragen
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/afragen/test-plugin2/
-GitHub Branch: develop
-Description: This plugin is used for testing functionality of Github updating of plugins.
-Version: 0.4.1
-Author: Andy Fragen
-License: GNU General Public License v2
-License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+GitHub Branch:     develop
 */
 
