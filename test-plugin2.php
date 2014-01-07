4bda164c720e1aad6bef3cddf7d4cea042038af5
Andy Fragen <andy@thefragens.com>
2014-01-06 16:10:59 -0800

    branch

--- test-plugin2.php
+++ test-plugin2.php
@@ -8,6 +8,6 @@ Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/afragen/test-plugin2/
-GitHub Branch:     develop
+GitHub Branch:     master
 */
 
