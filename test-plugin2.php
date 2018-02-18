8b9899d85a044913d81b72c0f8aa0d672076d4b6
Andy Fragen <andy@thefragens.com>
2018-02-18 09:12:06 -0800

    bump version number but not tag for endless cycle

--- test-plugin2.php
+++ test-plugin2.php
@@ -3,13 +3,11 @@
 Plugin Name:       Test Plugin2
 Plugin URI:        https://github.com/afragen/test-plugin2/
 Description:       This plugin is used for testing functionality of Github updating of plugins.
-Version:           0.4.8
+Version:           0.5.0
 Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/afragen/test-plugin2/
-GitHub Branch:     master
-Requires WP:       4.3
-Requires PHP:      5.4
+Requires WP:       4.8
+Requires PHP:      5.6
 */
-
